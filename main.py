64bc163979c148e34cb25b5809237bd7f78af8f8
Valishina Anna <valishina.anna@yandex.ru>
2025-04-17 22:35:23 +0300

    просто сохранить

--- main.py
+++ main.py
@@ -1,51 +1,47 @@
 import pandas as pd
+import time
+start = time.time()
+counter = 1
+def test(table):
+    global counter
 
 
-def quick_bag(index, target, extra_table):
-    extra_table = pd.DataFrame(extra_table)
-    extra_table.sort_values(by=extra_table.columns[1], ascending=[False], inplace=True)
+def dublicates(table):
+    global counter
+    for i in range(len(table)):
+        active = table.iloc[i]['Кредит (актив)']
+        index_active = table.iloc[i]['Новый индекс']
+        if index_active not in GLOBAL_used:
+            for j in range(i+1, len(table)):
+                passive = table.iloc[j]['Вклад (пассив)']
+                index_passive = table.iloc[j]['Новый индекс']
+                if index_passive not in GLOBAL_used and active != 0:
+                    if active + passive == 0:
 
-    current_sum = 0
-    used = [index]
-    best_diff = 10**20
+                        GLOBAL_used.append(index_passive)
+                        GLOBAL_used.append(index_active)
+                        data.at[index_passive, '[ОТВЕТ] Номер группы'] = counter
+                        data.at[index_active, '[ОТВЕТ] Номер группы'] = counter
+                        counter += 1
 
-    for row in extra_table.itertuples(index=False):
-        if abs((current_sum + row[2]) - target) < best_diff:
-            used.append(row[0])
-            #print(used)
-            current_sum += row[2]
-            best_diff = abs(current_sum - target)
+                    break
 
-        if best_diff == 0:
+    return table
+
+
+def main(data, days, rate, i):
+    extra_table = []
+    for j in range(i + 1, len(data)):
+        cur_index = data.index[j]
+        cur_active, cur_passive, cur_date, cur_days, cur_rate, cur_ans, cur_x = data.iloc[j]
+        if cur_rate - rate > 0.15:
             break
-    if target < (target - best_diff) * 1.4:
-        return used, best_diff
-    return [index], target
-
-
-def main(data, active_or_passive, index, active, passive,  date, rate):
-    extra_list = []
-    for stroka in data.itertuples(index=True):
-        next_index, next_active, next_passive, next_date, next_rate = stroka.Index, stroka._1, stroka._2, stroka._3, stroka._5
-
-        if abs(next_rate - rate) > 0.15:
-            break
-        if abs((next_date - date)) > pd.Timedelta(days=30) or next_index in GLOBAL_used:
+        if abs(cur_days - days) > 30 or cur_index in GLOBAL_used:
             continue
-        if active_or_passive == 'active':
-            if next_passive > 0:
-                extra_list.append(stroka)
-        else:
-            if next_active > 0 :
-                extra_list.append(stroka)
-    if len(extra_list) == 0:
-        return [index], active + passive
-    else:
-        if active_or_passive == 'active':
-            return quick_bag(index, active, extra_table=extra_list)
-        else:
-            return quick_bag(index, passive, extra_table=extra_list)
-
+        extra_table.append([cur_index, cur_active, -cur_passive, cur_date, cur_days, cur_rate, cur_ans, cur_x])
+    extra_table = [row] + extra_table
+    table = pd.DataFrame(extra_table, columns=['Новый индекс'] + list(data.columns))
+    return table
 
 
 data = pd.read_csv('Банк_3мес_данные.csv', sep=';', encoding='cp1251')
@@ -53,43 +49,34 @@ data['Вклад (пассив)'] = data['Вклад (пассив)'].str.replac
 data['Дата погашения'] = pd.to_datetime(data['Дата погашения'], format='%d.%m.%Y')
 
 
-#data.info()
+k = 1
 data.sort_values(by=['Ставка, %', 'Дата погашения'], ascending=[True, True], inplace=True) #сортировка
+
 
 left_active = 0
 left_passive = 0
-GLOBAL_used = set()
+GLOBAL_used = []
 RWA = 0
-for row in data.itertuples(index=True):
 
-    index, active, passive, date, rate = row.Index, row._1, row._2,row._3, row._5
-    if index not in GLOBAL_used:
-        if active > 0:
-            current_used, rwa = main(data, 'active', index, active, passive, date, rate)
-            if len(current_used) == 1:
-                left_active += rwa
-            else:
-                RWA += rwa
+for i in range(20):
+    index = data.index[i]
+    active, passive, date, days, rate, ans, x  = data.iloc[i]
+    passive = -passive
+    row = [index, active, passive, date, days, rate, ans, x]
+    table = main(data, days, rate, i)
 
 
 
+print(GLOBAL_used)
+#RWA += (left_active + left_passive) * 0.1 + abs(left_active - left_passive) * 0.4
 
-        else:
-            current_used, rwa = main(data, 'passive', index, active, passive, date, rate)
-            if len(current_used) == 1:
-                left_passive += rwa
-            else:
-                RWA += rwa
-        GLOBAL_used.update(current_used)
+end = time.time()
 
+print(f"Время выполнения: {end - start:.4f} секунд")
 
-RWA += (left_active + left_passive) * 0.1 + abs(left_active - left_passive) * 0.4
-
-print(RWA)
+print(data['[ОТВЕТ] Номер группы'].head(20))
 
 
 
 
-
-
-#data.to_csv('имя_файла111.csv', index=True, encoding='cp1251')
+data.to_csv('имя_файла2.csv', index=True, encoding='cp1251')
